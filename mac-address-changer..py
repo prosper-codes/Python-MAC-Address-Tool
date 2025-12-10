167b71bdb0e34ace0c9121c501f0425d97e53a4a
prosper-codes <tpjn02@gmail.com>
2025-12-10 03:50:23 +0100

    add variables for constants

--- mac-address-changer..py
+++ mac-address-changer..py
@@ -1,6 +1,12 @@
-#!/usr/bin/env python
 
 
 import subprocess
 
-subprocess.call("ifconfig", shell=True)
+interface="eth0",
+new_mac_add="00:22:33:55:77:99"
+print (f"[+] changing mac address of {interface} to {new_mac_add} ")
+subprocess.call(f"ifconfig {interface} down" , shell=True)
+subprocess.call(f"ifconfig {interface} hw ether {new_mac_add}" , shell=True)
+subprocess.call(f"ifconfig {interface} up" , shell=True)
+
+
