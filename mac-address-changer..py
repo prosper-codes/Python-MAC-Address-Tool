d0a5fd8161e1ddb0ff3c717633b5dc2725e169db
prosper-codes <tpjn02@gmail.com>
2025-12-10 04:09:25 +0100

    add user input

--- mac-address-changer..py
+++ mac-address-changer..py
@@ -2,8 +2,8 @@
 
 import subprocess
 
-interface="eth0",
-new_mac_add="00:22:33:55:77:99"
+interface="Type the interface>",
+new_mac_add=input("Enter the new MAC>")
 print (f"[+] changing mac address of {interface} to {new_mac_add} ")
 subprocess.call(f"ifconfig {interface} down" , shell=True)
 subprocess.call(f"ifconfig {interface} hw ether {new_mac_add}" , shell=True)
