2a381a17eceb9f87f641561326e72c139e8dae15
prosper-codes <tpjn02@gmail.com>
2025-12-10 17:23:57 +0100

    handle command line arguments and initialize variables based on command line Arguments

--- mac-address-changer..py
+++ mac-address-changer..py
@@ -1,12 +1,19 @@
-
-
 import subprocess
+import optparse
 
-interface="Type the interface>",
-new_mac_add=input("Enter the new MAC>")
+parser = optparse.OptionParser()
+
+parser.add_option("-i","--interface",dest="interface",help="Interface to change its mac address")
+parser.add_option("-m","--mac",dest="new_mac_add",help="New Mac address")
+
+(options ,arguments)=parser.parse_args()
+
+interface=options.interface
+new_mac_add=options.new_mac_add
+
 print (f"[+] changing mac address of {interface} to {new_mac_add} ")
-subprocess.call(f"ifconfig {interface} down" , shell=True)
-subprocess.call(f"ifconfig {interface} hw ether {new_mac_add}" , shell=True)
-subprocess.call(f"ifconfig {interface} up" , shell=True)
 
 
+subprocess.call(["ifconfig" , interface ,"down"])
+subprocess.call(["ifconfig" , interface, "hw","ether",new_mac_add])
+subprocess.call(["ifconfig", interface,"up"])
