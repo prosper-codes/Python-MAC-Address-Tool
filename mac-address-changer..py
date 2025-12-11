3528cdc515ec39d48f2a7a33495a1fbf29ecb324
prosper-codes <tpjn02@gmail.com>
2025-12-11 21:08:15 +0100

    handle errors

--- mac-address-changer..py
+++ mac-address-changer..py
@@ -1,19 +1,33 @@
 import subprocess
 import optparse
 
-def get_arguments ():
+
+def get_arguments():
     parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its mac address")
+    parser.add_option(
+        "-i",
+        "--interface",
+        dest="interface",
+        help="Interface to change its mac address",
+    )
     parser.add_option("-m", "--mac", dest="new_mac_add", help="New Mac address")
-    return parser.parse_args()
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error(
+            "[+] Please specify the interface you wish to change the mac address, use --help for more info"
+        )
+    elif not options.new_mac_add:
+        parser.error("[+] Please specify a new mac, use --help for more info")
+    return options
 
 
-
-def mac_changer(interface,new_mac_add):
+def mac_changer(interface, new_mac_add):
     print(f"[+] changing mac address of {interface} to {new_mac_add} ")
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac_add])
     subprocess.call(["ifconfig", interface, "up"])
 
-(options ,arguments)= get_arguments()
-mac_changer(options.interface,options.new_mac_add)
+
+options = get_arguments()
+
+mac_changer(options.interface, options.new_mac_add)
