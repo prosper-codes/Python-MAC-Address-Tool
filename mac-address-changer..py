ff48b605f9a131331b0107c6a67867682dc58ca8
prosper-codes <tpjn02@gmail.com>
2025-12-11 20:52:30 +0100

    make functions for cleaner code

--- mac-address-changer..py
+++ mac-address-changer..py
@@ -1,19 +1,19 @@
 import subprocess
 import optparse
 
-parser = optparse.OptionParser()
-
-parser.add_option("-i","--interface",dest="interface",help="Interface to change its mac address")
-parser.add_option("-m","--mac",dest="new_mac_add",help="New Mac address")
-
-(options ,arguments)=parser.parse_args()
-
-interface=options.interface
-new_mac_add=options.new_mac_add
-
-print (f"[+] changing mac address of {interface} to {new_mac_add} ")
+def get_arguments ():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its mac address")
+    parser.add_option("-m", "--mac", dest="new_mac_add", help="New Mac address")
+    return parser.parse_args()
 
 
-subprocess.call(["ifconfig" , interface ,"down"])
-subprocess.call(["ifconfig" , interface, "hw","ether",new_mac_add])
-subprocess.call(["ifconfig", interface,"up"])
+
+def mac_changer(interface,new_mac_add):
+    print(f"[+] changing mac address of {interface} to {new_mac_add} ")
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac_add])
+    subprocess.call(["ifconfig", interface, "up"])
+
+(options ,arguments)= get_arguments()
+mac_changer(options.interface,options.new_mac_add)
