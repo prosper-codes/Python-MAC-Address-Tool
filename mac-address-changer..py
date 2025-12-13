66c9a9bca79ca68a8ceda8bcf540e67d1458053e
prosper-codes <tpjn02@gmail.com>
2025-12-13 08:58:29 +0100

    add error checks and regex

--- mac-address-changer..py
+++ mac-address-changer..py
@@ -1,5 +1,6 @@
 import subprocess
 import optparse
+import re
 
 
 def get_arguments():
@@ -22,7 +23,7 @@ def get_arguments():
 
 
 def mac_changer(interface, new_mac_add):
-    print(f"[+] changing mac address of {interface} to {new_mac_add} ")
+    print(f"[+] changing mac address of {interface} to {new_mac_add}")
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac_add])
     subprocess.call(["ifconfig", interface, "up"])
@@ -31,5 +32,12 @@ def mac_changer(interface, new_mac_add):
 options = get_arguments()
 
 mac_changer(options.interface, options.new_mac_add)
-ifconf_output = subprocess.check_output(["ifconfig", options.interface])
-print(ifconf_output)
+
+ifconf_output = subprocess.check_output(["ifconfig", options.interface], text=True)
+mac_address_regrex_result = re.search(
+    r"\b[0-9A-Fa-f]{2}(:[0-9A-Fa-f]{2}){5}\b", ifconf_output
+)
+if mac_address_regrex_result:
+    print(mac_address_regrex_result.group(0))
+else:
+    print("[+] mac address not found")
