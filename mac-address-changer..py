d04c1c209550e83372455c54761266ed19dfebf5
prosper-codes <tpjn02@gmail.com>
2025-12-13 13:23:11 +0100

    improve with ai

--- mac-address-changer..py
+++ mac-address-changer..py
@@ -1,43 +1,113 @@
+#!/usr/bin/env python3
+
 import subprocess
 import optparse
 import re
+import os
+import sys
+import signal
+
+# ---------------------- Utility Checks ----------------------
+
+
+def is_root():
+    return os.geteuid() == 0
+
+
+def is_valid_mac(mac):
+    return re.fullmatch(r"[0-9A-Fa-f]{2}(:[0-9A-Fa-f]{2}){5}", mac) is not None
+
+
+def interface_exists(interface):
+    result = subprocess.run(
+        ["ip", "link", "show", interface],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+    return result.returncode == 0
+
+
+# ---------------------- Argument Parsing ----------------------
 
 
 def get_arguments():
     parser = optparse.OptionParser()
-    parser.add_option(
-        "-i",
-        "--interface",
-        dest="interface",
-        help="Interface to change its mac address",
-    )
-    parser.add_option("-m", "--mac", dest="new_mac_add", help="New Mac address")
-    (options, arguments) = parser.parse_args()
+    parser.add_option("-i", "--interface", dest="interface", help="Network interface")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    (options, _) = parser.parse_args()
+
     if not options.interface:
-        parser.error(
-            "[+] Please specify the interface you wish to change the mac address, use --help for more info"
-        )
-    elif not options.new_mac_add:
-        parser.error("[+] Please specify a new mac, use --help for more info")
+        parser.error("[-] Please specify an interface, use --help for info")
+    if not options.new_mac:
+        parser.error("[-] Please specify a new MAC address")
+    if not is_valid_mac(options.new_mac):
+        parser.error("[-] Invalid MAC address format (XX:XX:XX:XX:XX:XX)")
+
     return options
 
 
-def mac_changer(interface, new_mac_add):
-    print(f"[+] changing mac address of {interface} to {new_mac_add}")
-    subprocess.call(["ifconfig", interface, "down"])
-    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac_add])
-    subprocess.call(["ifconfig", interface, "up"])
+# ---------------------- MAC Operations ----------------------
 
 
-options = get_arguments()
+def get_current_mac(interface):
+    output = subprocess.check_output(["ip", "link", "show", interface], text=True)
+    match = re.search(r"link/ether\s([0-9A-Fa-f]{2}(:[0-9A-Fa-f]{2}){5})", output)
+    if match:
+        return match.group(1)
+    return None
 
-mac_changer(options.interface, options.new_mac_add)
 
-ifconf_output = subprocess.check_output(["ifconfig", options.interface], text=True)
-mac_address_regrex_result = re.search(
-    r"\b[0-9A-Fa-f]{2}(:[0-9A-Fa-f]{2}){5}\b", ifconf_output
-)
-if mac_address_regrex_result:
-    print(mac_address_regrex_result.group(0))
-else:
-    print("[+] mac address not found")
+def change_mac(interface, new_mac):
+    subprocess.run(["ip", "link", "set", interface, "down"], check=True)
+    subprocess.run(["ip", "link", "set", interface, "address", new_mac], check=True)
+    subprocess.run(["ip", "link", "set", interface, "up"], check=True)
+
+
+# ---------------------- Signal Handler ----------------------
+
+
+def setup_restore(original_mac, interface):
+    def restore(sig, frame):
+        print("\n[+] Restoring original MAC address...")
+        change_mac(interface, original_mac)
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, restore)
+
+
+# ---------------------- Main Logic ----------------------
+
+
+def main():
+    if not is_root():
+        print("[-] Please run this script as root")
+        sys.exit(1)
+
+    options = get_arguments()
+
+    if not interface_exists(options.interface):
+        print(f"[-] Interface {options.interface} does not exist")
+        sys.exit(1)
+
+    current_mac = get_current_mac(options.interface)
+    if not current_mac:
+        print("[-] Could not read current MAC address")
+        sys.exit(1)
+
+    print(f"[+] Current MAC: {current_mac}")
+
+    setup_restore(current_mac, options.interface)
+
+    print(f"[+] Changing MAC address of {options.interface} to {options.new_mac}")
+    change_mac(options.interface, options.new_mac)
+
+    new_mac = get_current_mac(options.interface)
+
+    if new_mac and new_mac.lower() == options.new_mac.lower():
+        print(f"[+] MAC successfully changed to {new_mac}")
+    else:
+        print("[-] MAC address change failed")
+
+
+if __name__ == "__main__":
+    main()
